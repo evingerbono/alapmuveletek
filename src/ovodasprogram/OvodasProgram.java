78785cef629c04ebb1dfe1a8ba0f9f9f85ff590e
frkdigo <95695550+frkdigo@users.noreply.github.com>
2022-03-09 15:19:21 +0100

    Add files via upload

---
+++ src/ovodasprogram/OvodasProgram.java
@@ -0,0 +1,79 @@
+package OvodasProgram;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class OvodasProgram {
+
+    public static void main(String[] args) {
+        int menu = beker(0, 100);
+
+    if (menu == 1) 
+        osszeadads();
+    else if (menu == 2)
+        kivonas();
+    else if (menu == 3) 
+        szorzás();
+    else if (menu == 4) 
+        osztas();
+}
+
+private static void osszeadads() {
+    Random rnd = new Random();
+    Scanner scr = new Scanner(System.in);
+    
+    int szam1 = rnd.nextInt(100), szam2 = rnd.nextInt(100), eredmeny = szam1 + szam2;
+    System.out.println(szam1 + " + " + szam2 + " = ?");
+    int valasz = scr.nextInt();
+    if (valasz == eredmeny)
+        System.out.println("Helyes válasz");
+    else
+        System.out.println("Rossz válasz! A helyes: "+ eredmeny);
+}
+
+
+
+private static void kivonas() {
+    Random rnd = new Random();
+    Scanner scr = new Scanner(System.in);
+    
+    int szam1 = rnd.nextInt(100), szam2 = rnd.nextInt(100), eredmeny = szam1 - szam2;
+    while(!(szam2 < szam1))
+        szam2 = rnd.nextInt(100);
+    System.out.println(szam1 + " - " + szam2 + " = ?");
+    int valasz = scr.nextInt();
+    if (valasz == eredmeny)
+        System.out.println("Helyes válasz");
+    else
+        System.out.println("Rossz válasz! A helyes: "+ eredmeny);
+}
+
+
+private static void szorzás() {
+    Random rnd = new Random();
+    Scanner scr = new Scanner(System.in);
+    int szam1 = rnd.nextInt(100), szam2 = rnd.nextInt(100), eredmeny = szam1 * szam2;
+    System.out.println(szam1 + " * " + szam2 + " = ?");
+    int valasz = scr.nextInt();
+    if (valasz == eredmeny)
+        System.out.println("Helyes válasz");
+    else
+        System.out.println("Rossz válasz! A helyes: "+ eredmeny);
+}
+
+
+
+private static void osztas() {
+    System.out.println("Osztas");
+}
+
+
+
+private static int beker(int also, int felso) {
+    Scanner scr = new Scanner(System.in);
+    System.out.print("Mit szeretnél gyakorolni? (1: Összeadás; 2: Kivonás; 3: Szorzás; 4: Osztás) ");
+    int szam = scr.nextInt();
+    return szam;
+}
+}
