282e234c7eced19747877facdd146e9fe0aaacf5
evingerbono <bono0823@gmail.com>
2022-03-09 15:48:23 +0100

    játék újra kezdése még nem jó

--- src/muveletek/Muveletek.java
+++ src/muveletek/Muveletek.java
@@ -8,23 +8,38 @@ import java.util.Scanner;
 public class Muveletek {
 
     public static void main(String[] args) {
-        int menu = beker(0, 100);
-
-    if (menu == 1) 
-        osszeadads();
-    else if (menu == 2)
-        kivonas();
-    else if (menu == 3) 
-        szorzás();
-    else if (menu == 4) 
-        osztas();
-}
-
-private static void osszeadads() {
-    System.out.println("Osszead");
-    Random rnd = new Random();
-    Scanner scr = new Scanner(System.in);
+         Scanner scrS = new Scanner(System.in);
     
+    boolean jatek = true;
+    while(jatek == true) {
+        int menu = beker(0, 100);
+        if (menu == 1) 
+            osszeadas();
+        else if (menu == 2)
+            kivonas();
+        else if (menu == 3) 
+            szorzás();
+        else if (menu == 4) 
+            osztas();
+        
+        System.out.print("Szeretnéd folytatni a játékot?(igen/nem) ");
+        String valasz = scrS.nextLine();
+        System.out.println(valasz);
+        
+        while(!(valasz.equals("igen")) || !(valasz.equals("nem"))){
+            System.out.print("Rosszul válaszoltál! Szeretnéd folytatni a játékot?(igen/nem)");
+            valasz = scrS.nextLine();
+        }
+        
+        if (valasz.equals("igen")) 
+            jatek = true;
+        else
+            jatek = false;
+    }
+    
+}
+private static void osszeadas() { Random rnd = new Random(); Scanner scr = new Scanner(System.in);
+
     int szam1 = rnd.nextInt(100), szam2 = rnd.nextInt(100), eredmeny = szam1 + szam2;
     System.out.println(szam1 + " + " + szam2 + " = ?");
     int valasz = scr.nextInt();
@@ -55,7 +70,6 @@ private static void kivonas() {
 
 
 private static void szorzás() {
-    System.out.println("Szorzas");
     Random rnd = new Random();
     Scanner scr = new Scanner(System.in);
     int szam1 = rnd.nextInt(100), szam2 = rnd.nextInt(100), eredmeny = szam1 * szam2;
@@ -65,13 +79,24 @@ private static void szorzás() {
         System.out.println("Helyes válasz");
     else
         System.out.println("Rossz válasz! A helyes: "+ eredmeny);
-
 }
 
 
 
 private static void osztas() {
-    System.out.println("Osztas");
+    Random rnd = new Random();
+    Scanner scr = new Scanner(System.in);
+    
+    int szam1 = rnd.nextInt(100), szam2 = rnd.nextInt(100);
+    while(!(szam2 < szam1))
+        szam2 = rnd.nextInt(100);
+    int eredmeny = eredmeny = szam1 / szam2;
+    System.out.println(szam1 + " / " + szam2 + " = ?");
+    int valasz = scr.nextInt();
+    if (valasz == eredmeny)
+        System.out.println("Helyes válasz");
+    else
+        System.out.println("Rossz válasz! A helyes: "+ eredmeny);
 }
 
 
