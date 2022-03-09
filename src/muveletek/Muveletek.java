bed62d8345cce0034ffdf7b449d702dc7adf1272
evingerbono <bono0823@gmail.com>
2022-03-09 14:25:58 +0100

    elso

---
+++ src/muveletek/Muveletek.java
@@ -0,0 +1,53 @@
+
+package muveletek;
+
+import java.util.Scanner;
+
+
+public class Muveletek {
+
+    public static void main(String[] args) {
+        int eredmeny;
+        int menu = beker();
+
+        beker();
+        if (menu == 1) {
+        osszeadads();
+        } else if (menu == 2) {
+        kivonas();
+        } else if (menu == 3) {
+        szorzás();
+        } else if (menu == 4) {
+        osztas();
+        }
+    
+ }
+    private static void osszeadads() {
+}
+
+
+
+private static void kivonas() {
+}
+
+
+
+private static void szorzás() {
+}
+
+
+
+private static void osztas() {
+}
+
+
+
+private static int beker(int also, int felso) {
+    Scanner scr = new Scanner(System.in);
+    int szam = scr.nextInt();
+    if (true) {
+
+    }
+return szam;
+}
+}
